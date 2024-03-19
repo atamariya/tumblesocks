7cf5c9fa1840dc264633de4fa7f96803023cab8b
Anand Tamariya <atamariya@gmail.com>
2024-03-19 11:11:37 +0530

    Initial version

---
+++ graph.el
@@ -0,0 +1,181 @@
+;;; graph.el --- Convenient graph for Emacs  -*- lexical-binding:t -*-
+
+;; Copyright (C) 2024 Free Software Foundation, Inc.
+
+;; Author: Anand Tamariya <atamarya@gmail.com>
+;; Keywords: graph, data visualization
+
+;; This file is not part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+(require 'gnuplot)
+
+(defun gnuplot--resolve-token (a)
+  (cond ((= (length a) 4) "Y")
+        ((= (length a) 3) "b") ;; Jan
+        ((> (string-to-number a) 31) "y")
+        ((> (string-to-number a) 12) "d")
+        (t "m")))
+
+(defun gnuplot--gen-datefmt (datetime &optional datetime1)
+  "Generate a gnuplot date format string or NIL if invalid."
+  (let* (;(tokens (parse-time-string (org-read-date nil nil datetime)))
+         (dash  (and (string-match-p "-" datetime) "-"))
+         (slash (and (string-match-p "/" datetime) "/"))
+         (time (string-match-p ":" datetime))
+         (sep (or dash slash))
+         (tokens1 (split-string datetime sep))
+         (tokens2 (split-string datetime1 sep))
+         res tokens3)
+    (when (or sep time)
+      ;; 1. Check for length
+      (setq out (mapcar 'gnuplot--resolve-token tokens1))
+      (pp out)
+      ;; 2. Use second date to disambiguate
+      (pcase-let
+          ((`(,s1 ,s2 ,s3) tokens1)
+           (`(,t1 ,t2 ,t3) tokens2))
+        (if (= (length s1) 4) (push "%Y" tokens3)
+          (if (string= s1 t1) (push "%m" tokens3) (push "%d" tokens3)))
+
+        (if (= (length s1) 3) (push "%b" tokens3) ;; Jan
+          (if (string= s2 t2) (push "%m" tokens3) (push "%d" tokens3)))
+
+        (when s3
+          (if (= (length s3) 4) (push "%Y" tokens3)
+            (if (string= s3 t3) (push "%m" tokens3) (push "%d" tokens3)))
+          ))
+      ;; (pcase-let
+      ;;     ((`(,s1 ,s2) tokens1)
+      ;;      (`(,t1 ,t2) tokens2))
+      ;;   (if (= (length s1) 4) (push "%Y" tokens3))
+      ;;   (if (string= s1 t1)   (push "%m" tokens3) (push "%d" tokens3))
+      ;;   (if (string= s2 t2)   (push "%m" tokens3) (push "%d" tokens3))
+      ;;   (if (= (length s2) 4) (push "%Y" tokens3))
+      ;;   )
+      (setq tokens3 (nreverse tokens3))
+      ;; (pp tokens)
+      (setq res (mapconcat 'identity tokens3 sep))
+      ;; (pcase-let
+      ;;     ((`(,sec ,min ,hour ,day ,mon ,year dow dst tz) tokens))
+      ;;   (setq res (apply 'concat
+      ;;                    (append (if mon  (list "%m" sep))
+      ;;                            (if day  (list "%d" sep))
+      ;;                            (if year (list year-str)))
+      ;;                    )))
+
+      ;; 3. Seek user help
+      (pcase-let
+          ((`(,s1 ,s2 ,s3) out))
+        (when (or (string= s1 s2)
+                  (string= s3 s2))
+          (if (string= s3 s2)
+              (if (string= s1 "Y")
+                  (setq out (list (mapconcat 'identity '("Y" "m" "d") sep)
+                                  (mapconcat 'identity '("Y" "d" "m") sep)
+                                  ))))
+          (if (string= s1 s2)
+              (if (string= s3 "Y")
+                  (setq out (list (mapconcat 'identity '("m" "d" "Y") sep)
+                                  (mapconcat 'identity '("d" "m" "Y") sep)
+                                  ))))
+          (pp out)
+          ))
+      )
+    res))
+
+;; gnuplot-selection
+;; styles lines, points, linespoints, impulses, dots, steps, errorbars (or
+;; yerrorbars), xerrorbars, xyerrorbars, boxes, boxerrorbars, boxxyerrorbars
+(defun gnuplot-rectangle (&optional title style)
+  (interactive)
+  (let* ((name (make-temp-file "plot"))
+         (data killed-rectangle)
+         (sep (and (string-match-p "," (car data)) ","))
+         (cols (split-string (car data) sep))
+         (n (length cols))
+         buf xlabel ylabel header j labelcol labelcol1 datetime)
+    ;; Extract plot details
+    ;; Column header
+    (when (setq labelcol1 (string-match-p "^[a-zA-Z]" (car cols)))
+      (if (= n 1)
+          (if (string-match-p "^[a-zA-Z]" (nth 1 data))
+              (error "Bad data")
+            (setq ylabel (car cols)
+                  labelcol nil))
+        (when (string-match-p "^[a-zA-Z]" (nth 1 cols))
+          (setq header cols
+                ;; style "histograms"
+                )
+          (pcase (length header)
+            (1 (setq ylabel (nth 1 header)))
+            (2 (setq xlabel (nth 0 header)
+                     ylabel (nth 1 header)))
+            (_ (setq xlabel (nth 0 header))))
+          ;; (pop killed-rectangle)
+          )))
+    ;; First column might be a label column or datetime
+    (setq labelcol (string-match-p "^[a-zA-Z]" (nth 1 data)))
+    (setq datetime (gnuplot--gen-datefmt (car (split-string (nth 1 data)))
+                                         (car (split-string (nth 2 data)))))
+
+    (setq buf (find-file name))
+    (with-current-buffer buf
+      (yank-rectangle)
+      (save-buffer))
+
+    (setq style (or style "line"))
+    (with-temp-buffer
+      (insert "unset xdata\n")
+      (insert "unset ydata\n")
+      (insert "unset xrange\n")
+      (insert "unset yrange\n")
+      (insert "unset format xy\n")
+      (insert "unset timefmt\n")
+
+      (insert "set datafile separator " (if sep "comma" "whitespace") "\n")
+      (insert "set title  '" (or title  "Title")  "'\n")
+      (insert "set xlabel '" (or xlabel "x-axis") "'\n")
+      (insert "set ylabel '" (or ylabel "y-axis") "'\n")
+      (when datetime
+        (insert "set xdata time\n")
+        (insert "set format x \"%b-%y\"\n")
+        (insert "set timefmt \"" datetime "\"\n"))
+      (insert "plot '" name "'")
+      (setq j (if (or labelcol datetime) 1 0))
+      (dotimes (i (- n j))
+        (insert " using ")
+        (when (not (string= style "histograms"))
+          (insert (number-to-string
+                   (if (or labelcol (= n 1)) 0 1))
+                  ":"))
+        (insert (number-to-string (+ i j 1)))
+        (if (and labelcol (not datetime)) (insert ":xtic(1)"))
+        (if (< i n) (insert " with " style))
+        (if (> n 2)
+            (insert " title columnhead")
+          (insert " notitle"))
+        (if (< i (- n j 1)) (insert ", ''")))
+      (newline)
+      (gnuplot-mode)
+      (gnuplot-send-buffer-to-gnuplot))
+
+    ;; Cleanup
+    (kill-buffer buf)
+    ;; (delete-file name)
+    ))
+
+
+(provide 'graph)
