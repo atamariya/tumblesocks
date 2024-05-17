ba7d2d17cc67b493154b7dc1cd50b71d5208966c
Anand Tamariya <atamariya@gmail.com>
2024-05-17 16:23:08 +0530

    Git graph

--- graph-brain.el
+++ graph-brain.el
@@ -161,6 +161,8 @@
   (pcase graph-brain--view
     ('graph-brain--view-tag
      (setq graph-brain--tags (butlast graph-brain--tags)))
+    ('graph-brain--view-wheel-git
+     (pop graph-brain--time))
     ('graph-brain--view-wheel
      (pop graph-brain--time)))
   (setq graph-brain--back t)
@@ -466,6 +468,120 @@
     (setq graph-brain--view 'graph-brain--view-tag)
     ))
 
+(defun graph-brain--view-wheel-git ()
+  (interactive)
+  (let* ((data (find-file-noselect "~/work/git-log.txt"))
+	 group filter nodes points title pt id lines files tmp name v fmt flag r)
+    ;; (with-current-buffer data
+    ;;   (goto-char (point-min))
+    ;;   (while (not (eobp))
+    ;; 	(message "%s" (current-line))
+    ;; 	(let ((p (point))
+    ;; 	      line)
+    ;; 	   (end-of-line)
+    ;; 	   (setq line (split-string (buffer-substring p (point)) ","))
+    ;; 	   (push (list (nth 0 line)
+    ;; 		       (parse-time-string (nth 1 line)))
+    ;; 		 files)
+    ;; 	   (forward-char))))
+
+    (catch 'node-view
+      (while (and ;files
+	      ;; (not flag)
+		  (or (not nodes)
+		      (and (= (length nodes) 1)
+			   (not graph-brain--back))))
+	(when (= (length nodes) 1)
+	  (push (car nodes) graph-brain--time))
+
+	(setq r (length graph-brain--time)
+	      fmt (or (graph-brain--get-date-format r)
+		      (and (setq flag t)
+			   '("%Y-%m-%d %H" . "%H")))
+	      filter (car graph-brain--time))
+
+	(if (= r 0)
+	    (let ((time (nth 5 (decode-time)))
+		  n s)
+	      (while (not (string= n "0"))
+		(message "%s" time)
+		(setq n (string-trim
+			 (shell-command-to-string
+			  (format "git rev-list --count --all --after='%s' --before='%s'"
+				  (1- time) time)))
+		      s (format "%s" time)
+		      time (1- time))
+		(push (list s s n) nodes))
+	      (pop nodes))
+
+	  (let (j before after time id title n cmd)
+	    (pcase r
+	      (1 (setq after  "%s-%02d-01 00:00"
+		       before "%s-%02d-31 00:00"
+		       j 12))
+	      (2 (setq after  "%s-%02d 00:00"
+		       before "%s-%02d 24:00"
+		       j 31))
+	      (3 (setq after  "%s %02d:00"
+		       before "%s %02d:00"
+		       j 24)))
+	    (dotimes (i j)
+	      (setq time (format after  filter (1+ i))
+		    cmd (format "git rev-list --count --all --after='%s' --before='%s'"
+				time (format before filter (1+ i))))
+	      (setq n (string-trim
+		       (shell-command-to-string cmd)))
+	      (message "%s %s" cmd n)
+	      (when (not (string= n "0"))
+		(setq id (format-time-string (car fmt) (date-to-time time))
+		      title (format-time-string (cdr fmt) (date-to-time time)))
+		(push (list id title n) nodes))))
+	;; (if flag (throw 'node-view t))
+	))
+
+    (dolist (p nodes)
+      (if flag
+	  (setq id (nth 0 p)
+		r 0
+		title (nth 1 p))
+	(setq id (nth 0 p)
+	      r (string-to-number (nth 2 p))
+	      title (format "%s (%d)" (nth 1 p) r)))
+      
+      (setq pt (make-point :x 0 :y 0 :r (+ r 30)
+			   :old-x 0 :old-y 0
+			   :fill (random-color-html)
+			   ;; :image (save-window-excursion
+			   ;; 	    (with-current-buffer
+			   ;; 		(graph-brain--open (concat "id:" (nth 0 p)))
+			   ;; 	      (car (org-property-values "IMAGE"))))
+			   :href (concat "time:" id)
+			   :text title
+			   :title (graph-brain--shorten title)))
+      (push (cons pt (car points)) lines)
+      (push pt points))
+    ;; (pp points)
+
+    (graph-brain--draw points (if (not flag) lines)
+		       (lambda (_d _i)
+			 (format "%s" (or (car graph-brain--time)
+					  "Timeline")))))
+
+    (when nil ;flag
+      ;; Render leaves
+      (setq filter (vconcat (mapcar (lambda (a)
+				      (car (nth 2 a)))
+				    (gethash (car graph-brain--time) group))))
+      (setq nodes (org-roam-db-query
+		   (vconcat
+		    [:select [id title] :from nodes]
+		    (vector :where `(in file ,filter)))
+		   ))
+      (graph-brain--view-node nodes))
+    (setq graph-brain--view 'graph-brain--view-wheel-git
+	  graph-brain--back nil)
+    ))
+
 (defun graph--firefox-bookmarks-list (tag)
   (let* ((buf (get-buffer-create "*bookmarks*")))
     (switch-to-buffer-other-window buf)
@@ -528,6 +644,7 @@
   (let* ((map (make-sparse-keymap)))
     (define-key map "/" 'graph-brain--search)
     (define-key map "d" 'graph-brain--node-delete)
+    (define-key map "G" 'graph-brain--view-wheel-git)
     (define-key map "w" 'graph-brain--view-wheel)
     (define-key map "g" 'graph-brain--view-group)
     (define-key map "n" 'graph-brain--view-node)
